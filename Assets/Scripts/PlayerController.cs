63122f1c9ce95279e7112737e8eb16b2f01a5ab5
web3ten0 <web3ten0@gmail.com>
2022-11-06 13:59:22 +0900

    8方向に歩く＆走れるようにした

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Settings
     public float Speed = 0.5f;
+
+    // Objects
     private Animator anim;
+    private PlayerInputsHandler inputs;
+
+    // States
+    private Vector2 currentVelocity;
 
     private void Awake()
     {
         anim = transform.GetComponentInChildren<Animator>();
+        inputs = GetComponent<PlayerInputsHandler>();
     }
 
     private void FixedUpdate()
     {
-        anim.SetFloat("Speed", Speed);
+        Vector2 direction = inputs.GetMove();
+        float speed = Mathf.Abs(direction.x) + Mathf.Abs(direction.y);
+        speed = Mathf.Clamp(speed, 0f, 1f);
+        speed = Mathf.SmoothDamp(anim.GetFloat("Speed"), speed, ref currentVelocity.y, 0.2f);
+        anim.SetFloat("Speed", speed);
+        anim.SetFloat("DirectionX", direction.x);
+        anim.SetFloat("DirectionY", direction.y);
     }
 }
--- Assets/Scripts/PlayerInputsHandler.cs
+++ Assets/Scripts/PlayerInputsHandler.cs
@@ -1,16 +1,107 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputsHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private PlayerInput _input;
+    private Vector2 _move, _look;
+    private bool _sprint, _crouch, _jump;
 
+    private void Awake()
+    {
+        TryGetComponent(out _input);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-
+        _input.actions["Move"].performed += OnMove;
+        _input.actions["Move"].canceled += OnMove;
+        _input.actions["Look"].performed += OnLook;
+        _input.actions["Look"].canceled += OnLook;
+        _input.actions["Sprint"].started += OnSprintStart;
+        _input.actions["Sprint"].canceled += OnSprintStop;
+        _input.actions["Crouch"].started += OnCrouchStart;
+        _input.actions["Crouch"].canceled += OnCrouchStop;
+        _input.actions["Jump"].started += OnJumpStart;
+        _input.actions["Jump"].canceled += OnJumpStop;
     }
+
+    private void OnDisable()
+    {
+        _input.actions["Move"].performed -= OnMove;
+        _input.actions["Move"].canceled -= OnMove;
+        _input.actions["Look"].performed -= OnLook;
+        _input.actions["Look"].canceled -= OnLook;
+        _input.actions["Sprint"].started -= OnSprintStart;
+        _input.actions["Sprint"].canceled -= OnSprintStop;
+        _input.actions["Crouch"].started -= OnCrouchStart;
+        _input.actions["Crouch"].canceled -= OnCrouchStop;
+        _input.actions["Jump"].started -= OnJumpStart;
+        _input.actions["Jump"].canceled -= OnJumpStop;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        _move = context.ReadValue<Vector2>();
+    }
+    private void OnLook(InputAction.CallbackContext context)
+    {
+        _look = context.ReadValue<Vector2>();
+    }
+
+    private void OnSprintStart(InputAction.CallbackContext context)
+    {
+        _sprint = true;
+    }
+
+    private void OnSprintStop(InputAction.CallbackContext context)
+    {
+        _sprint = false;
+    }
+
+    private void OnCrouchStart(InputAction.CallbackContext context)
+    {
+        _crouch = true;
+    }
+
+    private void OnCrouchStop(InputAction.CallbackContext context)
+    {
+        _crouch = false;
+    }
+
+    private void OnJumpStart(InputAction.CallbackContext context)
+    {
+        _jump = true;
+    }
+
+    private void OnJumpStop(InputAction.CallbackContext context)
+    {
+        _jump = false;
+    }
+
+    public Vector2 GetMove()
+    {
+        return _move;
+    }
+
+    public Vector2 GetLook()
+    {
+        return _look;
+    }
+
+    public bool GetSprint()
+    {
+        return _sprint;
+    }
+
+    public bool GetCrouch()
+    {
+        return _crouch;
+    }
+
+    public bool GetJump()
+    {
+        return _jump;
+    }
+
 }
